dbc5c0d1599e616261207cd33bb5c173866353c6
amazebhn <108827331+amazebhn@users.noreply.github.com>
2022-12-08 13:26:25 -0500

    Setup base files

---
+++ README.md
@@ -0,0 +1,2 @@
+Start project by running ```yarn start-api``` then opening new terminal and running ```yarn start```.
+> Make sure ports 5000 and 3000 are open
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
+  console.log(currentTime);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>The current time is {currentTime}.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
